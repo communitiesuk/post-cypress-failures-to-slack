8aee1c32e4417f45aadd134b7ebbecfadad4750d
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2022-02-10 15:55:37 +0000

    initial runnable action to read any log files and output count

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const walkSync = require('walk-sync');
+const { WebClient } = require('@slack/web-api')
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const token = core.getInput('token')
+    const channels = core.getInput('channels')
+    const workdir = core.getInput('workdir') || 'cypress'
+    const messageText =
+      core.getInput('message-text') ||
+      "A Cypress test just finished. I've placed the screenshots and videos in this thread. Good pie!"
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.debug(`Token: ${token}`)
+    core.debug(`Channels: ${channels}`)
+    core.debug(`Message text: ${messageText}`)
 
-    core.setOutput('time', new Date().toTimeString());
+    core.debug('Initializing slack SDK')
+    const slack = new WebClient(core.getInput('token'))
+    core.debug('Slack SDK initialized successfully')
+
+    core.debug('Checking for videos and/or screenshots from cypress')
+    const videos = walkSync(workdir, { globs: ['**/*.mp4'] })
+    const screenshots = walkSync(workdir, { globs: ['**/*.png'] })
+    const logs = walkSync(workdir, { globs: 'logs/*.json' })
+
+    core.info(`There were ${logs.length} errors based on the files present.`)
   } catch (error) {
     core.setFailed(error.message);
   }
