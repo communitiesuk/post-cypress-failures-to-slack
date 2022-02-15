cbbe198d72d2ab984947d27c6dac34fa1ae82ab1
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2022-02-15 10:44:46 +0000

    finesse failure rendering including failure count and not presenting thread when no videos/screenshots

--- src/format-failures-as-blocks.js
+++ src/format-failures-as-blocks.js
@@ -1,53 +1,62 @@
 /** Formats parsed failures as block components as per the Slack Block Kit @see https://api.slack.com/block-kit */
-const formatFailuresAsBlocks = (failures, messageText, videoCount, screenshotCount) => ([{
-  type: 'header',
-  text: {
-    type: 'plain_text',
-    text: messageText
-  }
-}].concat(
-  failures
-    .map(failure => ([
-      {
-        type: 'context',
-        elements: [
-          {
-            type: 'mrkdwn',
-            text: '📄'
-          },
-          {
-            type: 'mrkdwn',
-            text: `*File*: ${failure.testFile}`
-          }
-        ]
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `*failed test*: ${failure.fullDescription}`
-        }
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `*message*: \`${failure.message.split('\n')[0]}\``
-        }
-      },
-      {
-        type: 'divider'
-      }
-    ]))
-    .flat()
-).concat([
-  {
-    type: 'section',
+const formatFailuresAsBlocks = (failures, messageText, videoCount, screenshotCount) => {
+  let blocks = [{
+    type: 'header',
     text: {
-      type: 'mrkdwn',
-      text: `${videoCount === 1 ? 'One video' : `${videoCount} videos`} and ${screenshotCount === 1 ? 'one screenshot' : `${screenshotCount} screenshots`} in :thread:`
+      type: 'plain_text',
+      text: `${messageText} (${failures.length} failure${failures.length === 1 ? '' : 's'})`
     }
+  }].concat(
+    failures
+      .map(failure => ([
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: '📄'
+            },
+            {
+              type: 'mrkdwn',
+              text: `*File*: ${failure.testFile}`
+            }
+          ]
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*failed test*: ${failure.fullDescription}`
+          }
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*message*: \`${failure.message.split('\n')[0]}\``
+          }
+        },
+        {
+          type: 'divider'
+        }
+      ]))
+      .flat()
+  )
+
+  if (videoCount > 0 || screenshotCount > 0) {
+    blocks.push({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `${videoCount === 1 ? 'One video' : `${videoCount} videos`} and ${screenshotCount === 1 ? 'one screenshot' : `${screenshotCount} screenshots`} in :thread:`
+      }
+    })
+  } else {
+    // pop off the last divider
+    blocks.pop()
   }
-]))
+
+  return blocks
+}
 
 module.exports = formatFailuresAsBlocks
--- test/format-failures-as-blocks.test.js
+++ test/format-failures-as-blocks.test.js
@@ -20,7 +20,7 @@ test('it formats failures as expected Slack Block Kit blocks', () => {
       type: 'header',
       text: {
         type: 'plain_text',
-        text: 'Salieri has his 👀 on u'
+        text: 'Salieri has his 👀 on u (2 failures)'
       }
     },
     {
@@ -88,6 +88,89 @@ test('it formats failures as expected Slack Block Kit blocks', () => {
       text: {
         type: 'mrkdwn',
         text: '3 videos and one screenshot in :thread:'
+      }
+    }
+  ])
+})
+
+test('when there are no videos or screenshots it does not mention them or reference a thread', () => {
+  const failures = [
+    {
+      fullDescription: 'Check that there aren\'t too many notes',
+      message: 'There are too many notes',
+      testFile: 'salieri_spec.js'
+    },
+    {
+      fullDescription: 'Check again that there is the correct quantity of notes',
+      message: 'There are, still, too many notes',
+      testFile: 'salieri_reprise_spec.js'
+    }
+  ]
+  const messageText = 'Salieri has his 👀 on u'
+
+  expect(formatFailuresAsBlocks(failures, messageText, 0, 0)).toEqual([
+    {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: 'Salieri has his 👀 on u (2 failures)'
+      }
+    },
+    {
+      type: 'context',
+      elements: [
+        {
+          type: 'mrkdwn',
+          text: '📄'
+        },
+        {
+          type: 'mrkdwn',
+          text: '*File*: salieri_spec.js'
+        }
+      ]
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*failed test*: Check that there aren\'t too many notes'
+      }
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*message*: `There are too many notes`'
+      }
+    },
+    {
+      type: 'divider'
+    },
+    {
+      type: 'context',
+      elements: [
+        {
+          type: 'mrkdwn',
+          text: '📄'
+        },
+        {
+          type: 'mrkdwn',
+          text: '*File*: salieri_reprise_spec.js'
+        }
+      ]
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*failed test*: Check again that there is the correct quantity of notes'
+      }
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*message*: `There are, still, too many notes`'
       }
     }
   ])
